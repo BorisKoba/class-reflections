fad062ef9b46f6eb3d36855b5d9cc916fb6441a6
BorisKoba <browi@mail.ru>
2023-12-02 12:23:54 +0300

    CW 49

---
+++ src/telran/reflect/Test.java
@@ -0,0 +1,22 @@
+package telran.reflect;
+
+public class Test {
+	public void test1 (int a) {
+		System.out.println("test1, parametr with value " + a);
+	}
+	void test2 (int a) {
+		System.out.println("test2, parametr with value " + a);
+	}
+	void test3 (int a) {
+		System.out.println("test3, parametr with value " + a);
+	}
+	void test4 (int a) {
+		System.out.println("test4, parametr with value " + a);
+	}
+	void test5 (int a) {
+		System.out.println("test5, parametr with value " + a);
+	}
+	private void test6 (int a) {
+		System.out.println("test6, parametr with value " + a);
+	}
+}
